05e81b4a1c27460a1c1d1af561bac54a8ec9df64
Javier Ruiz <javierdeveloper1@gmail.com>
2022-07-04 20:29:06 +0200

    create trips and form edit trips

--- routes/trip.routes.js
+++ routes/trip.routes.js
@@ -8,22 +8,22 @@ router.get("/trips/create", (req, res, next) => res.render('trips/new-trip'))
 
 router.post("/trips/create", (req, res, next) => {
 
-    const { origin, destination, latitude, longitude, date, description, numberOfPassengers, smokingAllowed } = req.body
+    const { origin, destination, latitudeOrigin, longitudeOrigin, latitudeDestination, longitudeDestination, date, description, numberOfPassengers, smokingAllowed } = req.body
 
     Trip
         .create({
             origin: {
                 address: origin,
-                location: { type: 'Point', coordinates: [latitude, longitude] }
+                location: { type: 'Point', coordinates: [latitudeOrigin, longitudeOrigin] }
             },
 
             destination: {
                 address: destination,
-                location: { type: 'Point', coordinates: [latitude, longitude] }
+                location: { type: 'Point', coordinates: [latitudeDestination, longitudeDestination] }
             }, date, description, numberOfPassengers, smokingAllowed
         })
 
-        .then(trip => res.redirect('/'))
+        .then(trip => res.redirect('/trips'))
         .catch(err => console.log(err))
 })
 
@@ -49,6 +49,57 @@ router.get('/trips/:id', (req, res, next) => {
         })
         .catch(err => console.log(err))
 })
+
+//  Edit trips
+
+router.get('/trips/:id/edit', (req, res, next) => {
+
+    const { id } = req.params
+    console.log("hola aqui estamos")
+
+    Trip
+
+        .findById(id)
+        .then(trips => {
+            res.render('trips/edit-trip', { trips })
+        })
+        .catch(err => console.log(err))
+});
+
+router.post('/trips/:id/edit', (req, res, next) => {
+    const { id } = req.params
+    const { origin, destination, latitudeOrigin, longitudeOrigin, latitudeDestination, longitudeDestination, date, description, numberOfPassengers, smokingAllowed } = req.body
+
+    Trip
+        .findByIdAndUpdate(id, {
+            origin: {
+                address: origin,
+                location: { type: 'Point', coordinates: [latitudeOrigin, longitudeOrigin] }
+            },
+
+            destination: {
+                address: destination,
+                location: { type: 'Point', coordinates: [latitudeDestination, longitudeDestination] }
+            }, date, description, numberOfPassengers, smokingAllowed
+        })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+});
+
+
+// Delete trip
+
+/*
+router.post('/trips/:id/delete', (req, res) => {
+    const { id } = req.params
+
+    Trip
+        .findByIdAndDelete(id)
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+}) */
+
+
 
 
 module.exports = router
