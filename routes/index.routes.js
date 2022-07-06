44219d946d346748b51609b6004c9c431c91f3e4
Ghers Fisman <ghersf91@gmail.com>
2022-07-06 13:39:42 +0200

    rating form view and route done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -15,4 +15,7 @@ router.use('/api', require('./api.routes'))
 // User routes
 router.use('/', require('./user.routes'))
 
+// Rating routes
+router.use('/', require('./rating.routes'))
+
 module.exports = router
